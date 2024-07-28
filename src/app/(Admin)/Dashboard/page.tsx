93add82b62457b4d1db6be280367f2d3a0be0705
ozturk <ozturksirininfo@gmail.com>
2024-07-28 18:12:02 +0300

    chore: Update Dashboard page layout and component

--- src/app/(Admin)/Dashboard/page.tsx
+++ src/app/(Admin)/Dashboard/page.tsx
@@ -1,18 +1,11 @@
-import { SideBar } from "@/components";
 import React from "react";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
   return (
-    <div className="grid grid-cols-12 min-h-screen">
-      <div className="col-span-2 ">
-        <SideBar />
-      </div>
-      <div className="col-span-10 ">
-        <h1>Dashboard</h1>
-        <h1>Dashboard</h1>
-      </div>
+    <div>
+      <h1>DASHBOARD</h1>
     </div>
   );
 };
