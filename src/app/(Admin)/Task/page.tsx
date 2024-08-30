cad3407049942800675fb726c89258cdff72d206
ozturk <ozturksirininfo@gmail.com>
2024-08-30 22:39:13 +0300

    chore: Add lazy loading for images and update Seperator component in Dashboard page

--- src/app/(Admin)/Task/page.tsx
+++ src/app/(Admin)/Task/page.tsx
@@ -1,9 +1,42 @@
+"use client";
 import React from "react";
+import { Seperator, TaskProgresCard } from "@/components";
+import { TaskData } from "@/data/task";
 
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>page</div>;
+  const { timeLimit } = TaskData;
+  return (
+    <div>
+      <div>Search Input</div>
+      <div>
+        <Seperator
+          title="Time Limit"
+          children={
+            <div className="flex">
+              {timeLimit.map((task) => {
+                return (
+                  <TaskProgresCard
+                    key={task.key}
+                    taskTitle={task.taskTitle}
+                    jobTitle={task.jobTitle}
+                    progress={task.progress}
+                    daysLeft={task.daysLeft}
+                    person={
+                      task.person?.map((person) => {
+                        return person;
+                      }) || []
+                    }
+                  />
+                );
+              })}
+            </div>
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ src/data/task.ts
@@ -0,0 +1,63 @@
+import PersonOne from "@/assets/icons/photo-1.svg";
+import PersonTwo from "@/assets/icons/photo-2.svg";
+
+export const TaskData = {
+  timeLimit: [
+    {
+      key: "1",
+      taskTitle: "Creating Mobile App Design",
+      jobTitle: "UI UX Design",
+      progress: 66,
+      daysLeft: "2 ",
+      person: [PersonOne, PersonTwo],
+    },
+    {
+      key: "2",
+      taskTitle: "Creating Perfect Website",
+      jobTitle: "Web Developer",
+      progress: 81,
+      daysLeft: "4 ",
+      person: [PersonTwo, PersonOne],
+    },
+    {
+      key: "3",
+      taskTitle: "Creating Perfect Website",
+      jobTitle: "Web Developer",
+      progress: 81,
+      daysLeft: "4 ",
+      person: [PersonTwo, PersonOne],
+    },
+    {
+      key: "4",
+      taskTitle: "Creating Perfect Website",
+      jobTitle: "Web Developer",
+      progress: 81,
+      daysLeft: "4 ",
+      person: [PersonTwo, PersonOne],
+    },
+    {
+      key: "5",
+      taskTitle: "Creating Perfect Website",
+      jobTitle: "Web Developer",
+      progress: 81,
+      daysLeft: "4 ",
+      person: [PersonTwo, PersonOne],
+    },
+    {
+      key: "6",
+      taskTitle: "Creating Perfect Website",
+      jobTitle: "Web Developer",
+      progress: 81,
+      daysLeft: "4 ",
+      person: [PersonTwo, PersonOne],
+    },
+    {
+      key: "7",
+      taskTitle: "Creating Perfect Website",
+      jobTitle: "Web Developer",
+      progress: 81,
+      daysLeft: "4 ",
+      person: [PersonTwo, PersonOne],
+    },
+  ],
+} as const;
